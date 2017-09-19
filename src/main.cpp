0a01531bcf75c42844d4ae330cb2134dd2c82452
Szwarc Adam <adam.szwarc@nokia.com>
2017-09-20 00:44:12 +0200

    Separate mains for tests and source

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,20 @@
-#define CATCH_CONFIG_MAIN
-#include "catch.hpp"
+#include "area_calculator.hpp"
+
+#include <iostream>
+
+int main()
+{
+    const char data[] = {
+        1, 0, 0, 20,
+        2, 10, 10, 20, 30,
+        3, 6, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6,
+        4, -10, -10, 15
+    };
+    int dataSize{27};
+    AreaCalculator calculator;
+    int result = calculator.calculateArea(data, dataSize);
+
+    std::cout << "Area of figures is: " << result << std::endl;
+
+    return 0;
+}
